e005105086fc293a191cab0b34e1b72be6997454
Mickey <michael.cook@techdept.co.uk>
2022-03-25 19:59:13 +0000

    Feels memory leaky but hey ho!

--- behaviours/Controllable.ts
+++ behaviours/Controllable.ts
@@ -120,44 +120,79 @@ export default class {
     }
   }
 }
+const directionKeycodeFromTouchPos = ({ x, y, width, height }) => {
+  // Much improved rewrite, with advice from https://gist.github.com/fbacall
+  let keyCode = "none";
+  const relX = x - width / 2;
+  const relY = y - height / 2;
+  if (Math.abs(relY) > Math.abs(relX)) {
+    keyCode = relY < 0 ? "w" : "s";
+  } else {
+    keyCode = relX > 0 ? "ArrowRight" : "ArrowLeft";
+  }
+  return keyCode;
+};
 export const initTouchEvents = (controls) => {
-  document.body.addEventListener(
-    "wheel",
-    (e) => {
-      e.preventDefault();
-      // Two-finger scroll
-      // if (Math.random() > 0.99) console.log(e.deltaY);
-      // Debounce can't be (this.) because the scope is lost
-      // Also argh, there is no end gesture for wheel
-      if (e.deltaY || e.deltaX) {
-        if (e.deltaY > 0) {
-          clearTimeout((window as any).touchDebounceForward);
-          controls.keyFind("w").pressed = true;
-          (window as any).touchDebounceForward = setTimeout(() => {
-            controls.keyFind("w").pressed = false;
-          }, 100);
-        } else if (e.deltaY < 0) {
-          clearTimeout((window as any).touchDebounceBackward);
-          controls.keyFind("s").pressed = true;
-          (window as any).touchDebounceBackward = setTimeout(() => {
-            controls.keyFind("s").pressed = false;
-          }, 100);
-        }
-        if (e.deltaX > 20) {
-          clearTimeout((window as any).touchDebounceLeft);
-          controls.keyFind("ArrowLeft").pressed = true;
-          (window as any).touchDebounceLeft = setTimeout(() => {
-            controls.keyFind("ArrowLeft").pressed = false;
-          }, 100);
-        } else if (e.deltaX < -20) {
-          clearTimeout((window as any).touchDebounceRight);
-          controls.keyFind("ArrowRight").pressed = true;
-          (window as any).touchDebounceRight = setTimeout(() => {
-            controls.keyFind("ArrowRight").pressed = false;
-          }, 100);
-        }
-      }
-    },
-    { passive: false }
-  );
+  document.body.addEventListener("touchstart", (e) => {
+    e.preventDefault(); // plus user-select: none;
+    const dir = directionKeycodeFromTouchPos({
+      x: e.touches[0].pageX,
+      y: e.touches[0].pageY,
+      width: document.body.clientWidth,
+      height: document.body.clientHeight,
+    });
+    controls.keyFind(dir).pressed = true;
+    setTimeout(() => {
+      kill(); // JIC
+    }, 3000);
+  });
+  const kill = () => {
+    controls.keyFind("w").pressed = false;
+    controls.keyFind("s").pressed = false;
+    controls.keyFind("ArrowLeft").pressed = false;
+    controls.keyFind("ArrowRight").pressed = false;
+  };
+  document.body.addEventListener("touchend", (e) => {
+    kill();
+  });
+  // Wheel was aight on dekstop, but no joy on mobile. Should have tankbladed earlier!
+  // document.body.addEventListener(
+  //   "wheel",
+  //   (e) => {
+  //     e.preventDefault();
+  //     // Two-finger scroll
+  //     // if (Math.random() > 0.99) console.log(e.deltaY);
+  //     // Debounce can't be (this.) because the scope is lost
+  //     // Also argh, there is no end gesture for wheel
+  //     if (e.deltaY || e.deltaX) {
+  //       if (e.deltaY > 0) {
+  //         clearTimeout((window as any).touchDebounceForward);
+  //         controls.keyFind("w").pressed = true;
+  //         (window as any).touchDebounceForward = setTimeout(() => {
+  //           controls.keyFind("w").pressed = false;
+  //         }, 100);
+  //       } else if (e.deltaY < 0) {
+  //         clearTimeout((window as any).touchDebounceBackward);
+  //         controls.keyFind("s").pressed = true;
+  //         (window as any).touchDebounceBackward = setTimeout(() => {
+  //           controls.keyFind("s").pressed = false;
+  //         }, 100);
+  //       }
+  //       if (e.deltaX > 20) {
+  //         clearTimeout((window as any).touchDebounceLeft);
+  //         controls.keyFind("ArrowLeft").pressed = true;
+  //         (window as any).touchDebounceLeft = setTimeout(() => {
+  //           controls.keyFind("ArrowLeft").pressed = false;
+  //         }, 100);
+  //       } else if (e.deltaX < -20) {
+  //         clearTimeout((window as any).touchDebounceRight);
+  //         controls.keyFind("ArrowRight").pressed = true;
+  //         (window as any).touchDebounceRight = setTimeout(() => {
+  //           controls.keyFind("ArrowRight").pressed = false;
+  //         }, 100);
+  //       }
+  //     }
+  //   },
+  //   { passive: false }
+  // );
 };
